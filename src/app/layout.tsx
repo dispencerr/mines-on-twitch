8ba3fe08f59c140a23eb7b30ee631b66a65b02be
dispencerr <di.spencerr@gmail.com>
2024-01-26 17:27:30 +0900

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "destyle.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Mines on Twitch",
-  description: "Minesweeper game with Twitch integration",
+  description: "Minesweeper-like game with Twitch integration",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
