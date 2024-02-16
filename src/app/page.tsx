309e74d061aa089adb4556edb64e4ad00e0aabd7
dispencerr <di.spencerr@gmail.com>
2024-02-16 17:36:00 +0900

    refactor: match connection logic to Wordle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,55 +8,79 @@ import StartingScreen from "@/app/components/StartingScreen";
 import { Client } from "tmi.js";
 import tmi from "tmi.js";
 
+const CONNECTION_RETRY_INTERVAL = 500; // Amount to wait between connection attempts, in ms
+const MAX_CONNECTION_TRIES = 5; // Number of times to try connecting before giving up
+
 export default function Home() {
   const searchParams = useSearchParams();
-  const [getClient, setClient] = useState<Client | null>(null);
-  const [getChannel, setChannel] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isConnecting, setIsConnecting] = useState(false);
-  const [isConnected, setIsConnected] = useState(false);
+  const [getClient, setClient] = useState<Client | null>(null); // The Client object containing the channel to connect to using the Twitch chat API
+  const [getChannel, setChannel] = useState<string | null>(null); // The Twitch channel to connect to
+  const [isLoading, setIsLoading] = useState(true); // If the game is in the loading state
+  const [isConnecting, setIsConnecting] = useState(false); // If the game is attempting to connect
+  const [isConnected, setIsConnected] = useState(false); // If the game has successfully connected
 
+  /**
+   * Set the game up in offline mode (change the connected state to true but keep the client as null)
+   */
   const playOffline = () => {
     setIsConnected(true);
   };
 
-  const connectToChannel = (channel: string) => {
-    setChannel(channel);
+  /**
+   * Check the connection to the specified channel at set intervals and update the state and the URL on a successful connection
+   *
+   * @param {Client} client - The client object to conncet to
+   */
+  const testConnection = (client: Client): void => {
+    setIsConnecting(true);
+    // eslint-disable-next-line prefer-const
+    let tryConnection: NodeJS.Timeout;
+    let connectionTries = 0;
+
+    const checkConnection = () => {
+      if (client.getChannels().length > 0) {
+        // Connection was successful
+        setIsConnected(true);
+        clearInterval(tryConnection);
+
+        // Update the browser URL
+        const params = new URLSearchParams(searchParams.toString());
+        params.set("channel", client.getChannels()[0].slice(1));
+        window.history.pushState(null, "", `?${params.toString()}`);
+      } else if (connectionTries >= MAX_CONNECTION_TRIES) {
+        // Connection was unsuccessful and tried too many times
+        clearInterval(tryConnection);
+        alert("Connection failed");
+        setChannel("");
+        setIsConnecting(false);
+        const params = new URLSearchParams(searchParams.toString());
+        params.delete("channel");
+        const queryString = params.toString();
+        const newUrl = queryString ? `?${queryString}` : "/";
+        window.history.pushState(null, "", newUrl);
+        setClient(null);
+      } else {
+        // Connection was unsuccessful, increase the count of attempts so far
+        connectionTries++;
+      }
+    };
+
+    tryConnection = setInterval(checkConnection, CONNECTION_RETRY_INTERVAL);
   };
 
+  /**
+   * When the channel is set, update the Client object in the state and attempt to connect.
+   */
   useEffect(() => {
-    if (getClient) {
-      setIsConnecting(true);
-      // eslint-disable-next-line prefer-const
-      let tryConnection: NodeJS.Timeout;
-      let connectionTries = 0;
-
-      const checkConnection = () => {
-        if (getClient.getChannels().length > 0) {
-          setIsConnected(true);
-          clearInterval(tryConnection);
-          const params = new URLSearchParams(searchParams.toString());
-          params.set("channel", getClient.getChannels()[0].slice(1));
-          window.history.pushState(null, "", `?${params.toString()}`);
-        } else if (connectionTries >= 5) {
-          clearInterval(tryConnection);
-          alert("Connection failed");
-          setChannel("");
-          setIsConnecting(false);
-          const params = new URLSearchParams(searchParams.toString());
-          params.delete("channel");
-          const queryString = params.toString();
-          const newUrl = queryString ? `?${queryString}` : "/";
-          window.history.pushState(null, "", newUrl);
-          setClient(null);
-        } else {
-          connectionTries++;
-        }
-      };
-
-      tryConnection = setInterval(checkConnection, 500);
+    if (getChannel) {
+      const client: Client = new tmi.Client({
+        channels: [getChannel],
+      });
+      setClient(client);
+      client.connect();
+      testConnection(client);
     }
-  }, [getClient]);
+  }, [getChannel]);
 
   useEffect(() => {
     if (getChannel) {
@@ -68,8 +92,10 @@ export default function Home() {
     }
   }, [getChannel]);
 
+  /**
+   * Parse the URL parameters to get the "channel" parameter and connect if it exists
+   */
   useEffect(() => {
-    // Parse the URL parameters to get the "channel" parameter
     const searchParams = new URLSearchParams(location.search);
     const channelParam = searchParams.get("channel");
 
@@ -87,7 +113,7 @@ export default function Home() {
           !isLoading ? (
             <>
               <StartingScreen
-                connectToChannel={connectToChannel}
+                connectToChannel={setChannel}
                 playOffline={playOffline}
               />
               <a
